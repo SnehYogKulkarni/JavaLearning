e4d105dbf796891671cc83410e6cdeb3b3b875dc
SnehYogKulkarni <115880442+SnehYogKulkarni@users.noreply.github.com>
2022-10-29 17:45:32 +0530

    Add files via upload

---
+++ JavaVariables.java
@@ -0,0 +1,23 @@
+
+public class JavaVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare variables and assign valuesint 
+		int num1 = 10;
+		float num2 = 12;
+		char character = 'A'; 
+		boolean bool = true;
+		String str = "Great Learning";
+		double num3 = 10.23;
+		//display all the variable on the console
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(character);
+		System.out.println(bool);
+		System.out.println(str);
+		System.out.println(num3);
+
+	}
+
+}
