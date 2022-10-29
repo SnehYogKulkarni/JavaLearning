3458e0c499519e31a914f190a225cb9f2553e497
SnehYogKulkarni <115880442+SnehYogKulkarni@users.noreply.github.com>
2022-10-29 17:53:24 +0530

    Add files via upload

---
+++ JavaOperators.java
@@ -0,0 +1,26 @@
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//declare variables of type float 
+		float num1, num2, result;
+		//assign values to variables num1, num2 '
+		num1= 10;num2= 1;
+		//store the operation result in variable 
+		result= num1+ num2;
+		//display the result 
+		System.out.println("Sum "+result);
+		result= num1-num2;
+		//display the result 
+		System.out.println("Difference "+result);
+		result= num1* num2;
+		//display the result 
+		System.out.println("Multiplication "+result);
+		result= num1/ num2;
+		//display the result 
+		System.out.println("Division "+result);
+	}
+
+}
