797ca3386388f530e844803b595affb9852196ca
SnehYogKulkarni <115880442+SnehYogKulkarni@users.noreply.github.com>
2022-10-29 18:52:12 +0530

    Add files via upload

---
+++ ConditionalExpression.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class ConditionalExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in); 
+		
+		System.out.println("Enter the Number");
+		
+		int a = sc.nextInt();
+		
+	    if (a%2 == 0)
+	    {
+	    	System.out.println(" the number " + a + " is an even number");
+	    	
+	    }
+		
+	    else 
+	    	
+	    	System.out.println(" the number " + a + " is an odd number");
+
+	}
+}
